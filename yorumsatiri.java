be9da0cec4f9fdda8fd7e529124d91e9fc5911dd
dogankeser <89547015+dogankeser@users.noreply.github.com>
2021-08-25 22:50:35 +0300

    Update yorumsatiri.java

--- yorumsatiri.java
+++ yorumsatiri.java
@@ -1,1 +1,3 @@
-
+System.out.println("Merhaba tekstil"); // Bu tek satırlık açıklama satırıdır. Bu şekilde kısa açıklama yapılabilir. Sitenin kullanıcı kısmında görünmez.
+/* Bu çok satırlı açıklama satırıdır. Bu şekilde uzun açıklama yapılabilir. Sitenin kullanıcı kısmında görünmez. */
+System.out.println("Elveda yazlım");
