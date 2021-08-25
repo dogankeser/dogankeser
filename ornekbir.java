7634c6822b116e9be20923dd369dd175ecd802c9
dogankeser <89547015+dogankeser@users.noreply.github.com>
2021-08-25 22:46:57 +0300

    Update ornekbir.java

--- ornekbir.java
+++ ornekbir.java
@@ -1,5 +1,6 @@
-/* Ekrana Yazı yazdırmak. */
-public class Main {
+/* Ekrana Yazı yazdırmak. java dosyasının adıyla aynı bir class açılması gerekiyor.  */
+public class Ornekbir {
+  /*Aşağıdaki main ifadesinin kullanımı zorunludur. Bunun dışında */
   public static void main(String[] args) {
     /* Ekrana yazı yazdırmak için aşağıdaki kod parçacığını kullanabiliriz. Çift tırnaklar yerine tek tırnak kullanılabilir.*/
     System.out.println("Merhaba tekstil, elveda yazılım ve öğretmenlik.");
