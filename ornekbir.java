ddc2301d503934355718642235b5b4080f8640cb
dogankeser <89547015+dogankeser@users.noreply.github.com>
2021-08-25 22:39:43 +0300

    Update ornekbir.java
    Ekrana yazı yazdırma 
    Print to screen

--- ornekbir.java
+++ ornekbir.java
@@ -1,1 +1,7 @@
-
+/* Ekrana Yazı yazdırmak. */
+public class Main {
+  public static void main(String[] args) {
+    /* Ekrana yazı yazdırmak için aşağıdaki kod parçacığını kullanabiliriz. Çift tırnaklar yerine tek tırnak kullanılabilir.*/
+    System.out.println("Merhaba tekstil, elveda yazılım ve öğretmenlik.");
+  }
+}
